927399c03536c425884713313b67240cb4ef0e5d
DemirBalemir <demirbalemir@gmail.com>
2023-06-03 21:47:03 +0300

    devam

--- src/utilz/LoadSave.java
+++ src/utilz/LoadSave.java
@@ -5,14 +5,19 @@ import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 
+import entities.Crabby;
 import main.Game;
+
+import static utilz.Constants.EnemyConstants.CRABBY;
 
 public class LoadSave {
 
-	public static final String PLAYER_ATLAS = "theone-removebg-preview.png";
+
+	public static final String PLAYER_ATLAS = "brotherman.png";
 	public static final String LEVEL_ATLAS = "outside_sprites.png";
 	public static final String LEVEL_ONE_DATA = "level_one_data_long.png";
 	public static final String MENU_BUTTONS = "button_atlas.png";
@@ -23,6 +28,9 @@ public class LoadSave {
 	public static final String VOLUME_BUTTONS = "volume_buttons.png";
 	public static final String MENU_BACKGROUND_IMG = "orman2.png";
 	public static final String PLAYING_BG_IMAGE = "jungle-1.png";
+	public static final String CRABBY_SPRITE = "crabby_sprite.png";
+	public static final String STATUS_BAR = "health_power_bar.png";
+
 
 	
 	public static BufferedImage GetSpriteAtlas(String fileName) {
@@ -43,10 +51,24 @@ public class LoadSave {
 		return img;
 	}
 
+	public static ArrayList<Crabby> GetCrabs() {
+		BufferedImage img = GetSpriteAtlas(LEVEL_ONE_DATA);
+		ArrayList<Crabby> list = new ArrayList<>();
+		for (int j = 0; j < img.getHeight(); j++)
+			for (int i = 0; i < img.getWidth(); i++) {
+				Color color = new Color(img.getRGB(i, j));
+				int value = color.getGreen();
+				if (value == CRABBY)
+					list.add(new Crabby(i * Game.TILES_SIZE, j * Game.TILES_SIZE));
+			}
+		return list;
+
+	}
+
 	public static int[][] GetLevelData() {
-		
 		BufferedImage img = GetSpriteAtlas(LEVEL_ONE_DATA);
 		int[][] lvlData = new int[img.getHeight()][img.getWidth()];
+
 		for (int j = 0; j < img.getHeight(); j++)
 			for (int i = 0; i < img.getWidth(); i++) {
 				Color color = new Color(img.getRGB(i, j));
